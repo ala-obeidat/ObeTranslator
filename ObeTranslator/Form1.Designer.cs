658383b5a9541344917f868c139c950ad2c51fe8
Ala Obeidat <ala-work@hotmail.com>
2020-06-03 21:33:07 +0300

    Upload Files
    First uploading

---
+++ ObeTranslator/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ObeTranslator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dialogFilePath = new System.Windows.Forms.OpenFileDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ckbEnglishMeaning = new System.Windows.Forms.CheckBox();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dialogFilePath
+            // 
+            this.dialogFilePath.DefaultExt = "txt";
+            this.dialogFilePath.FileName = "openFileDialog1";
+            this.dialogFilePath.Filter = "Text files (*.txt)|*.txt";
+            this.dialogFilePath.RestoreDirectory = true;
+            this.dialogFilePath.Title = "Browse Text Files";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(607, 66);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(110, 30);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browes...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFilePath.Location = new System.Drawing.Point(36, 66);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(565, 30);
+            this.txtFilePath.TabIndex = 1;
+            this.txtFilePath.Text = "file path";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text File";
+            // 
+            // ckbEnglishMeaning
+            // 
+            this.ckbEnglishMeaning.AutoSize = true;
+            this.ckbEnglishMeaning.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckbEnglishMeaning.Location = new System.Drawing.Point(36, 163);
+            this.ckbEnglishMeaning.Name = "ckbEnglishMeaning";
+            this.ckbEnglishMeaning.Size = new System.Drawing.Size(196, 30);
+            this.ckbEnglishMeaning.TabIndex = 3;
+            this.ckbEnglishMeaning.Text = "English meaning";
+            this.ckbEnglishMeaning.UseVisualStyleBackColor = true;
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTranslate.Location = new System.Drawing.Point(268, 156);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(126, 44);
+            this.btnTranslate.TabIndex = 4;
+            this.btnTranslate.Text = "Translate";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(36, 234);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(681, 48);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(36, 307);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ShowSelectionMargin = true;
+            this.txtLog.Size = new System.Drawing.Size(681, 307);
+            this.txtLog.TabIndex = 6;
+            this.txtLog.Text = "";
+            this.txtLog.TextChanged += new System.EventHandler(this.txtLog_TextChanged);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.Location = new System.Drawing.Point(432, 163);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(21, 29);
+            this.lblCount.TabIndex = 7;
+            this.lblCount.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 636);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnTranslate);
+            this.Controls.Add(this.ckbEnglishMeaning);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Obe Translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog dialogFilePath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox ckbEnglishMeaning;
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.Label lblCount;
+    }
+}
+
---
+++ ObeTranslator/Form1.cs
@@ -0,0 +1,249 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObeTranslator
+{
+    public partial class Form1 : Form
+    {
+        public string FilePath { get; set; }
+        public List<string> Words { get; set; }
+        Regex SearchRegex = new Regex("<span class=\"def\" hclass=\"def\" htag=\"span\">(.*?)(</span>|<a)", RegexOptions.IgnoreCase);
+        Regex SearchRegex2 = new Regex("<span class=\"def\" (.*?)(</span>|<a)", RegexOptions.IgnoreCase);
+        Regex SpanRegex = new Regex("<span class=\"def\" hclass=\"def\" htag=\"span\">", RegexOptions.IgnoreCase);
+        Regex SpanEndRegex = new Regex("<(.*?)>", RegexOptions.IgnoreCase);
+        Regex CommaRegex = new Regex(",", RegexOptions.IgnoreCase);
+
+        private static readonly HttpClient client = new HttpClient();
+        private static readonly HttpClient Postclient = new HttpClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+
+            if (dialogFilePath.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = FilePath = dialogFilePath.FileName;
+                Log("Getting Text file...");
+                using (var file = new System.IO.StreamReader(FilePath))
+                {
+                    Log("Reading the file...");
+                    string word = string.Empty;
+                    Words = new List<string>();
+                    while ((word = file.ReadLine()) != null)
+                    {
+                        if (string.IsNullOrEmpty(word) || string.IsNullOrWhiteSpace(word))
+                            continue;
+                        Words.Add(word.Trim());
+                    }
+                    Words = Words.Distinct().ToList();
+                    Log($"File Readed with ({Words.Count}) words");
+                }
+            }
+            else
+            {
+                Error("Browse File: Can't Read The file ...");
+            }
+        }
+
+        private void Error(string error)
+        {
+            Log(error);
+            MessageBox.Show(error, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void Log(string error)
+        {
+            MethodInvoker(() => txtLog.Text += "\n" + error, txtLog);
+        }
+        private void IncreaseCount(int count)
+        {
+            MethodInvoker(() => lblCount.Text = $"{count} of {Words.Count}", lblCount);
+        }
+        private void ProgreesIncrease()
+        {
+            MethodInvoker(() => progressBar1.Value += 1, progressBar1);
+        }
+        private void ProgreesReset(int max)
+        {
+            MethodInvoker(() =>
+            {
+                progressBar1.Minimum = 0;
+                progressBar1.Maximum = max;
+            }, progressBar1);
+        }
+        private void MethodInvoker(Action method, Control control)
+        {
+            MethodInvoker mi = new MethodInvoker(method);
+            if (control.InvokeRequired)
+            {
+                control.Invoke(mi);
+            }
+            else
+            {
+                mi.Invoke();
+            }
+        }
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append("The word,English Meaning 1,English Meaning 2,Arabic Meaning\n");
+            ProgreesReset(Words.Count);
+            Log("**** Start Translate ....");
+            Postclient.BaseAddress = new Uri("https://www.translate.com");
+            int count = 0;
+            foreach (var word in Words)
+            {
+                Log("Translate " + word);
+                string translatedWord = word;
+                string line = word;
+                if (word.IndexOf(',') > -1)
+                {
+                    translatedWord = word.Substring(0, word.IndexOf(','));
+                }
+
+                if (ckbEnglishMeaning.Checked)
+                {
+                    Log("Get English meaning for " + translatedWord);
+                    line += "," + GetEnglishMeaning(translatedWord);
+                }
+                Log("Get Arabic meaning for " + translatedWord);
+                line += "," + GetArabicMeaning(translatedWord);
+                result.Append(line + "\n");
+                ProgreesIncrease();
+                Log($"({translatedWord}) Added to list");
+                Thread.Sleep(50);
+                count++;
+                IncreaseCount(count);
+            }
+            var fileName = Path.GetFileNameWithoutExtension(FilePath);
+            var newFilePath = FilePath.Replace(fileName + ".txt", fileName + "_New.csv");
+            if (File.Exists(newFilePath))
+            {
+                File.Delete(newFilePath);
+            }
+            Log("Saving file with path " + newFilePath);
+            using (var sr = new StreamWriter(newFilePath, true, System.Text.Encoding.UTF8))
+            {
+                sr.Write(result.ToString());
+            }
+            Log("******* DONE ********");
+            MessageBox.Show("Done");
+        }
+
+        private string GetArabicMeaning(string word)
+        {
+            try
+            {
+                var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("text_to_translate", word),
+                new KeyValuePair<string, string>("source_lang", "en"),
+                new KeyValuePair<string, string>("translated_lang", "ar"),
+                new KeyValuePair<string, string>("use_cache_only", "false"),
+            });
+
+                var result = Postclient.PostAsync("/translator/ajax_translate", content).Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    string resultContent = result.Content.ReadAsStringAsync().Result;
+                    TranslateArabic resultText = JsonConvert.DeserializeObject<TranslateArabic>(resultContent);
+                    if (resultText.result == "success")
+                    {
+                        return resultText.translated_text;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log($"ERROR ON WORD: {word} |{ex.Message}");
+            }
+            return "ERROR";
+        }
+
+        private string GetEnglishMeaning(string word)
+        {
+            try
+            {
+                var result = new List<string>();
+
+                var responseString = client.GetStringAsync($"https://www.oxfordlearnersdictionaries.com/definition/english/" + word + "_1?q=" + word).Result;
+                MatchCollection means = null;
+
+                if (SearchRegex.IsMatch(responseString))
+                {
+                    means = SearchRegex.Matches(responseString);
+                }
+                else if (SearchRegex2.IsMatch(responseString))
+                {
+                    means = SearchRegex2.Matches(responseString);
+                }
+                else  
+                {
+                    responseString = client.GetStringAsync($"https://www.oxfordlearnersdictionaries.com/definition/academic/" + word).Result;
+                    if (SearchRegex.IsMatch(responseString))
+                    {
+                        means = SearchRegex.Matches(responseString);
+                    }
+                    else if (SearchRegex2.IsMatch(responseString))
+                    {
+                        means = SearchRegex2.Matches(responseString);
+                    }
+                }
+                int maxCount = 2;
+                int count = 0;
+                foreach (var item in means)
+                {
+                    if (maxCount == count)
+                    {
+                        break;
+                    }
+                    count++;
+                    result.Add(SpanRegex.Replace(SpanEndRegex.Replace(CommaRegex.Replace(item.ToString(), ";"), ""), "").Replace("<span class=\"dh\">","").Replace("<a",""));
+                }
+                if (result.Count == 0)
+                {
+                    result.Add("");
+                }
+                if (result.Count == 1)
+                {
+                    result.Add("");
+                }
+                return string.Join(",", result);
+            }
+            catch (Exception ex)
+            {
+                Log($"ERROR ON WORD: {word} |{ex.Message}");
+            }
+            return "ERROR";
+        }
+
+        private void txtLog_TextChanged(object sender, EventArgs e)
+        {
+            // set the current caret position to the end
+            txtLog.SelectionStart = txtLog.Text.Length;
+            // scroll it automatically
+            txtLog.ScrollToCaret();
+        }
+    }
+    public class TranslateArabic
+    {
+        public string result { get; set; }
+        public string translated_text { get; set; }
+    }
+}
